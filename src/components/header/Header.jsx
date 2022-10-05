d0cb55ece57bfc65930a325b4c2fbf84c2b37722
NanatsuYorou <ruslanisp555@gmail.com>
2022-10-06 00:06:39 +0500

    small fixes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,23 +9,54 @@ import remindersSVG from '../../images/icon-reminders.svg'
 import planningSVG from '../../images/icon-planning.svg'
 import calendarSVG from '../../images/icon-calendar.svg'
 
+
 export const Header = () => {
     
-const [menuIsShown, toggleMenu] = useState(false)
+const [menuIsShown, showMenu] = useState(false)
+
     
-function burgerClick(){
-    let menu_list = document.querySelector(`.${css['menu__list']}`)
+function toggleMenu(){
+    let menu_list = document.querySelector(`.${css['menu__wrapper']}`)
     menu_list.classList.toggle(css['active'])
-    toggleMenu(!menuIsShown)
+
+    let menu__background = document.querySelector(`.${css['menu__background']}`)
+    menu__background.classList.toggle(css['active'])
+
+    closeThem()
+
+    showMenu(!menuIsShown)
+}
+
+function closeThem(){
+    let array = document.querySelectorAll(`.${css['open']}`)
+    for (let i = 0; i < array.length; i++) {
+        array[i].classList.toggle(css['open'])
+    }
 }
 
 function openSublist(event){
+    // Массив уже открытых подсписков
+    let open_sublists = document.querySelectorAll(`ul.${css['open']}`)
+    // Id текущего подсписка
+    let sublistId = event.currentTarget.id
+
+    // Раскрыть связанный с нажатым пунктом по data-id подсписок 
+    let sublist = document.querySelector(`[data-id="${sublistId}"]`)
+    sublist.classList.toggle(css['open'])
+
+    // Закрыть все открытые подсписки кроме текущего
+    for (let i = 0; i < open_sublists.length; i++) {
+        if(open_sublists[i].dataset.id !== sublistId)
+            open_sublists[i].classList.toggle(css['open'])
+    }
+
     // Перевернуть стрелку нажатого пункта
     event.currentTarget.classList.toggle(css['open'])
-    // Раскрыть связанный с нажатым пунктом по data-id подсписок 
-    let sublistClass = event.currentTarget.id
-    let sublist = document.querySelector(`[data-id="${sublistClass}"]`)
-    sublist.classList.toggle(css['open'])
+
+    if(window.innerWidth >= 767){
+        document.querySelector(`.${css['menu__background']}`).classList.add(css['active'])
+    }
+
 }
 
     return(
@@ -35,38 +66,41 @@ function openSublist(event){
                     <img src={logo} alt="" />
                 </div>
                 <div className={css["header__menu"]}>
-                    <ul className={css["menu__list"]}>
-                        <li className={css["menu__item"]}>  
-                            <p id="features" onClick={openSublist}>Features</p>
-                            <img src={arrowSVG} alt="" />
-                            <ul data-id="features" className={css["sublist"] + ' ' + css["features"]}>
-                                <li style={{listStyleImage: `url(${todoSVG})`}} className={css["sublist__item"]}><a href="">Todo List</a></li>
-                                <li style={{listStyleImage: `url(${calendarSVG})`}} className={css["sublist__item"]}><a href="">Calendar</a></li>
-                                <li style={{listStyleImage: `url(${remindersSVG})`}} className={css["sublist__item"]}><a href="">Reminders</a></li>
-                                <li style={{listStyleImage: `url(${planningSVG})`}} className={css["sublist__item"]}><a href="">Planning</a></li>
-                            </ul>
-                        </li>
-                        <li className={css["menu__item"]}>  
-                            <p id="company" onClick={openSublist}>Company</p>
-                            <img src={arrowSVG} alt="" />
-                            <ul data-id="company" className={css["sublist"] + ' ' + css["company"]}>
-                                <li className={css["sublist__item"]}><a href="">History</a></li>
-                                <li className={css["sublist__item"]}><a href="">Our Blog</a></li>
-                                <li className={css["sublist__item"]}><a href="">Team</a></li>
-                            </ul>   
-                        </li>
-                        <li className={css["menu__item"]}>  
-                            <p><a href="">Careers</a></p>
-                        </li>
-                        <li className={css["menu__item"]}>  
-                            <p><a href="">About</a></p>
-                        </li>
-                        <li className={css["menu__item"]}>
+                    <div className={css["menu__wrapper"]}>
+                        <ul className={css["menu__list"]}>
+                            <li className={css["menu__item"]}>  
+                                <p id="features" onClick={openSublist}>Features</p>
+                                <img src={arrowSVG} alt="" />
+                                <ul data-id="features" className={css["sublist"]}>
+                                    <li style={{listStyleImage: `url(${todoSVG})`}} className={css["sublist__item"]}><a href="">Todo List</a></li>
+                                    <li style={{listStyleImage: `url(${calendarSVG})`}} className={css["sublist__item"]}><a href="">Calendar</a></li>
+                                    <li style={{listStyleImage: `url(${remindersSVG})`}} className={css["sublist__item"]}><a href="">Reminders</a></li>
+                                    <li style={{listStyleImage: `url(${planningSVG})`}} className={css["sublist__item"]}><a href="">Planning</a></li>
+                                </ul>
+                            </li>
+                            <li className={css["menu__item"]}>  
+                                <p id="company" onClick={openSublist}>Company</p>
+                                <img src={arrowSVG} alt="" />
+                                <ul data-id="company" className={css["sublist"]}>
+                                    <li className={css["sublist__item"]}><a href="">History</a></li>
+                                    <li className={css["sublist__item"]}><a href="">Our Blog</a></li>
+                                    <li className={css["sublist__item"]}><a href="">Team</a></li>
+                                </ul>   
+                            </li>
+                            <li className={css["menu__item"]}>  
+                                <p><a href="">Careers</a></p>
+                            </li>
+                            <li className={css["menu__item"]}>  
+                                <p><a href="">About</a></p>
+                            </li>
+                        </ul>
+                        <div className={css["menu__footer"]}>
                             <a href="">Login</a>
                             <a href="">Register</a>
-                        </li>
-                    </ul>
-                    <div className={css["menu__burger"]} onClick={burgerClick}>
+                        </div>
+                    </div>
+                    <div className={css['menu__background']} onClick={toggleMenu}></div>
+                    <div className={css["menu__burger"]} onClick={toggleMenu}>
                     { menuIsShown ? <img src={closeMenuSVG} alt="" /> : <img src={menuSVG} alt="" />}
                     </div>
                 </div>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import databiz_svg from "../../images/client-databiz.svg";
 import audiophile_svg from "../../images/client-audiophile.svg";
 import maker_svg from "../../images/client-maker.svg";
 import meet_svg from "../../images/client-meet.svg";
-import background_png from '../../images/image-hero-mobile.png'
 import css from './main-mobile.module.css'
+import backgroundMobile from '../../images/image-hero-mobile.png'
+import backgroundDesktop from '../../images/image-hero-desktop.png'
 
 export const Main = () => {
+
+    const [mobileDevice, changeToMobile] = useState(null)
+
+    // Не работает динамическая смена картинки, подправить
+    useEffect(()=>{
+        if(window.innerWidth >= '767'){
+            console.log("it's a desktop width")
+            changeToMobile(false)
+        } else{
+            console.log("it's a mobile width")
+            changeToMobile(true)
+        }
+    }, [window.innerWidth])
+
     return(
         <main>
-            <img src={background_png} className={css["background"]} alt="" />
+            <div className={css["background"]}>
+               {mobileDevice ? <img id="mobile" src={backgroundMobile} alt="" /> : <img id="desktop" src={backgroundDesktop} alt="" /> }
+            </div>
             <section>
-            <h2>Make remote work</h2>
-            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-            <button>Learn more</button>
+                <h2>Make remote work</h2>
+                <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+                <button >Learn more</button>
+                <footer>
+                    <img src={databiz_svg} alt="" />
+                    <img src={audiophile_svg} alt="" />
+                    <img src={meet_svg} alt="" />
+                    <img src={maker_svg} alt="" />
+                </footer>
             </section>
-            <footer>
-            <img src={databiz_svg} alt="" />
-            <img src={audiophile_svg} alt="" />
-            <img src={meet_svg} alt="" />
-            <img src={maker_svg} alt="" />
-            </footer>
         </main>
     )
 }
